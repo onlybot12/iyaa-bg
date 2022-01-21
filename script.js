33e6c4aca10d840b5c1876520a62cee8c385a985
Ibnu Nur Ramadani <92385634+IniIbnu@users.noreply.github.com>
2022-01-21 23:36:21 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,83 +1,276 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+<!DOCTYPE html>
+<!-- Created By Ibnu Nur'R -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Web</title>
+    <link rel="stylesheet" href="https://iniibnu.github.io/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Ibnu<span>Nur'R</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Services</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#teams" class="menu-btn">Teams</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
 
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, nama ku</div>
+                <div class="text-2">IbnuGanz (i.n.r) Ibnu Nur'R</div>
+                <div class="text-3">Dan aku adalah <span class="typing"></span></div>
+                <a href="/docs">Rest Api</a>
+            </div>
+        </div>
+    </section>
 
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About Me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="https://avatars.githubusercontent.com/iniibnu" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">Saya Ibnu Nur'R dan saya <span class="typing-2"></span></div>
+                    <p>Halo! Saya Ibnu Nur'R. Saya belajar pemrograman dan peretasan ketika saya berusia 12 tahun | Bahasa yang baru saya pelajari : JavaScript, HTML, CSS, Python, PHP.</p>
+                    <a href="https://iniibnu.github.io/profile.html">My Profile Card</a>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Pelajar Smp N1 Jeruklegi", "Murid Kelas IXE", "Apps/Web Designer", "Creator Bot"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My Services</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Bot Developer</div>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Rest Api</div>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Web Designer</div>
+                        <p></p>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section>
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Murid Smp N1 Jeruklegi", "Discord Bot Developer", "Apps/Web Designer", "Whatsapp Bot Developer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">My creative skills & experiences.</div>
+                    <p></p>
+                   <!-- <a href="#">Read more</a>-->
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>5%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>15%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                   <!-- <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>-->
+                </div>
+            </div>
+        </div>
+    </section>
 
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-});
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">My teams</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="https://avatars.githubusercontent.com/IniIbnu" alt="">
+                        <div class="text">Ibnu Nur'R</div>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="https://iniibnu.github.io/images/profile-2.jpeg" alt="">
+                        <div class="text">idk</div>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="https://avatars.githubusercontent.com/caliph91" alt="">
+                        <div class="text">Caliph</div>
+                        <p></p>
+                    </div>
+               <!-- </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-4.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/profile-5.jpeg" alt="">
+                        <div class="text">Someone name</div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>-->
+            </div>
+        </div>
+    </section>
 
-let subject = document.querySelector('.subject');
-let body = document.querySelector('.message');
-    let searchBtn = document.querySelector('.searchBtn');
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>If there is a need contact me</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Ibnu Nur'R</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">Cilacap,Jawa Tengah</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">ibnunurramadani175@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" class="subject" value="" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Message.." class="message" value="" required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit" class="searchBtn">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="abc">
+            <a class="a" href="https://wa.me/+628989031500">
+              <i class="fab fa-whatsapp"></i>
+            </a>
+            <a class="a" href="https://github.com/IniIbnu">
+              <i class="fab fa-github"></i>
+            </a>
+            <a class="a" href="https://instagram.com/pencari_meme.id">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a class="a" href="https://youtube.com/channel/UCXMoWTiVrwaHkhL0UayDxVA">
+              <i class="fab fa-youtube"></i>
+            </a>
+          </div>
+    </section>
     
-    searchBtn.onclick = function(){
-        let url = 'mailto:adiicotz@gmail.com?subject='+subject.value;
-        let url2 = '&body='+message.value;
-        window.open(url+url2,'_self');
-    }
+
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://iniibnu.github.io/profile.html">Ibnu Nur'R</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+    </footer>
+
+    <script src="https://iniibnu.github.io/script.js"></script>
+</body>
+<audio autoplay="autoplay" loop="loop" controls="controls" hidden="hidden" src="../music/stay.mp3" type="audio/mpeg"/>
+</html>
