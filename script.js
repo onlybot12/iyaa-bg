3a39a4db0a40e63239c2b7022f5aca4cd6d91634
Ibnu Nur Ramadani <92385634+IniIbnu@users.noreply.github.com>
2022-01-21 22:25:55 +0700

    update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Gamer", "meme", "pengocok", "Bot"],
+        strings: ["Murid Smp N1 Jeruklegi", "Pelajar Kelas 9E", "Wibu Lokal 9E"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Gamer", "meme", "pengocok", "Bot"],
+        strings: ["Murid Smp N1 Jeruklegi", "Pelajar Kelas 9E", "Wibu Lokal 9E"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
